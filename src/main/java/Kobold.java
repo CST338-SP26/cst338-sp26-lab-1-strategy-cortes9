487efdf202b52a66e91d89ecf1f14f1f7242ae03
cortes9 <lcortescedillo@csumb.edu>
2026-03-01 18:44:35 -0800

    Kobold

---
+++ src/main/java/Kobold.java
@@ -0,0 +1,13 @@
+package edu.csumb;
+
+public class Kobold extends edu.csumb.Monster {
+
+    public Kobold(Integer hp, Integer maxHP) {
+        super(hp, maxHP);
+    }
+
+    @Override
+    public String toString() {
+        return null;
+    }
+}
